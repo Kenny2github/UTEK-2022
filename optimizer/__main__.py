5853e7ddd54f5363d2ecd8786866c74d527054ce
AbyxDev <ken@abyx.dev>
2022-01-15 16:46:39 -0500

    Some running convenience

--- optimizer/__main__.py
+++ optimizer/__main__.py
@@ -1,4 +1,9 @@
 import sys
 from optimizer import run1, run2, run3, run4, run5
 
+if len(sys.argv) > 2:
+    sys.stdin = open(sys.argv[2])
+if len(sys.argv) > 3:
+    sys.stdout = open(sys.argv[3], 'w')
+
 globals()['run' + sys.argv[1]]()
